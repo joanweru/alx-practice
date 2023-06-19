df883bf48bac9bc870c9735d30271e2ce1134a68
joanweru <nyokabiweru44@gmail.com>
2023-06-19 10:05:05 +0300

    scanf

---
+++ c-practice/scanf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - blah blah
+ * Return: blah blah
+ */
+
+int main(void)
+{
+int num1;
+
+printf("Enter number: ");
+scanf("%d", &num1);
+printf("Number picked is: %d",num1);
+return (0);
+}
